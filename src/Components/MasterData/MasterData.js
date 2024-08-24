a5350e126b643a227c59b9bcb8c30f313b7da44f
sridhar <sridharbe4ui@gmail.com>
2024-08-24 16:12:55 +0530

    file add

--- src/Components/MasterData/MasterData.js
+++ src/Components/MasterData/MasterData.js
@@ -1,10 +1,14 @@
 import React from "react";
 import RegionTable from './Region/Region';
+import GodownTable from './Godown/GodownTable';
+import GodownInput from './Godown/GodownInput';
 
-const MasteData = () => {
+const MasteData = ({options}) => {
     return(
         <>
             <RegionTable />
+            <GodownTable />
+            <GodownInput />
         </>
     )
 }
